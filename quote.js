22f2396a1f7ff427cac51e5307ddd483c8e9a665
Pearl Robin <pearl@devacademy.co.nz>
2016-04-07 11:29:23 +1200

    Pearl's 2nd half hour of driving

--- quote.js
+++ quote.js
@@ -1,35 +1,39 @@
-var quotes = [{
+var quoteArray = [{
   "quote": "The pistachio: it's just like our politics. When the two sides are divided, that's when the nuts come out.",
-  "source": "Stephen Colbert"
+  "author": "Stephen Colbert"
 }, {
   "quote": "People usually consider walking on water or in thin air a miracle. But I think the real miracle is not to walk on water or in thin air, but to walk on earth.",
-  "source": "Thich Nhat Hanh"
+  "author": "Thich Nhat Hanh"
 }, {
   "quote": "All that is gold does not glitter; not all those who wander are lost; the old that is strong does not wither; deep roots are not reached by the frost.",
-  "source": "J.R.R. Tolkien"
+  "author": "J.R.R. Tolkien"
 }, {
   "quote": "Parrots make great pets. They have more personality than goldfish.",
-  "source": "Chevy Chase"
+  "author": "Chevy Chase"
 }, {
   "quote": "Dying is a wild night and a new road.",
-  "source": "Emily Dickinson"
+  "author": "Emily Dickinson"
 }, {
   "quote": "Always remember that you are absolutely unique. Just like everyone else.",
-  "source": "Margaret Mead"
+  "author": "Margaret Mead"
 }, {
   "quote": "Life isn't something you possess. It's something you take part in, and you witness.",
-  "source": "Louis C. K."
+  "author": "Louis C. K."
 }, {
   "quote": "Kia mate ururoa, kei mate wheke. (Fight like a shark, don’t give in like an octopus.)",
-  "source": "Maori Proverb"
+  "author": "Maori Proverb"
 }, {
   "quote": "Ahakoa iti, he pounamu. (Although it is small, it is of greenstone.)",
-  "source": "Maori Proverb"
+  "author": "Maori Proverb"
 }, {
   "quote": "To infinity, and beyond!",
-  "source": "Buzz Lightyear"
+  "author": "Buzz Lightyear"
 }]
 
 function newQuote() {
-
+  var quoteObject = quoteArray[Math.floor(Math.random() * quoteArray.length)]
+  document.getElementById('quote-box').innerHTML = '<p>"' + quoteObject.quote + '"</p>' 
+  document.getElementById('author-box').innerHTML = '<p> - ' + quoteObject.author + '</p>'
 }
+
+newQuote()
