b1f2b3d79bc9a1e6b98d62a6c5394bd90ebe65b5
Broc Harcourt <broc.harcourt@gmail.com>
2016-04-07 11:05:09 +1200

    add JSON quotes x10

---
+++ quote.js
@@ -0,0 +1,35 @@
+var quotes = [{
+  "quote": "The pistachio: it's just like our politics. When the two sides are divided, that's when the nuts come out.",
+  "source": "Stephen Colbert"
+}, {
+  "quote": "People usually consider walking on water or in thin air a miracle. But I think the real miracle is not to walk on water or in thin air, but to walk on earth.",
+  "source": "Thich Nhat Hanh"
+}, {
+  "quote": "All that is gold does not glitter; not all those who wander are lost; the old that is strong does not wither; deep roots are not reached by the frost.",
+  "source": "J.R.R. Tolkien"
+}, {
+  "quote": "Parrots make great pets. They have more personality than goldfish.",
+  "source": "Chevy Chase"
+}, {
+  "quote": "Dying is a wild night and a new road.",
+  "source": "Emily Dickinson"
+}, {
+  "quote": "Always remember that you are absolutely unique. Just like everyone else.",
+  "source": "Margaret Mead"
+}, {
+  "quote": "Life isn't something you possess. It's something you take part in, and you witness.",
+  "source": "Louis C. K."
+}, {
+  "quote": "Kia mate ururoa, kei mate wheke. (Fight like a shark, don’t give in like an octopus.)",
+  "source": "Maori Proverb"
+}, {
+  "quote": "Ahakoa iti, he pounamu. (Although it is small, it is of greenstone.)",
+  "source": "Maori Proverb"
+}, {
+  "quote": "To infinity, and beyond!",
+  "source": "Buzz Lightyear"
+}]
+
+function newQuote() {
+
+}
